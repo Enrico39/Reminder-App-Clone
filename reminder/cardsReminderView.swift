bb284f52396556f1e91ef4e3725b024b4e4cc544
Enrico <enricomadonna0@gmail.com>
2023-11-14 15:12:59 +0100

    main page with reminders collection - interface

---
+++ reminder/cardsReminderView.swift
@@ -0,0 +1,18 @@
+//
+//  cardsReminderView.swift
+//  reminder
+//
+//  Created by Enrico Madonna on 14/11/23.
+//
+
+import SwiftUI
+
+struct cardsReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    cardsReminderView()
+}
